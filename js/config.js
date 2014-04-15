53c0838b749c0fbd6f6e2cd0d407fd01e244aaa4
Ellen <ellenm1@gmail.com>
2014-04-15 17:54:59 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,36 @@
-//Directions for filling this out:
-//https://github.com/adlnet/xAPIWrapper#configuration
+if (typeof console == "undefined" || typeof console.log == "undefined") var console = { log: function() {} }; 
+var testing = true; //turn to true to show debugging information for this script
 
-ar conf = {
+ 
+
+var conf = {
   "endpoint" : "https://cloud.scorm.com/ScormEngineInterface/TCAPI/public/",
-  
+ 
 };
 
-ADL.XAPIWrapper.changeConfig(conf);
+
  
+var adl = ADL.XAPIWrapper;
+adl.debugLevel = "info";
+var myactivity = "http://mlearning.med.umich.edu/ct2/cbtlib/modules/test/tincan/adl/bootstrap_adl/";
+var myactid = "http://example.com/activity/courses/xapi";
+var myagent = {"mbox" : "mailto:tester@example.com","name":"Joe Tester","objectType": "Agent"};
+var xapiMasteryScore = '80';
+
+var myprofile = {
+    "umid": "123456799",
+    "roles": [
+        "Nurse",
+        "isORstaff",
+        "patientCareArea",
+        "centralLineProc",
+        "isResearcher"
+    ]
+}
+ 
+ 
+//https://npmjs.org/package/adl-xapiwrapper
+
+
+//ADL.XAPIWrapper.changeConfig(conf);
+adl.changeConfig(conf);
